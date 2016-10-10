111d98ab4c2158afb3c1ef6616c724618845edd6
reddeals <nobrother@gmail.com>
2016-10-10 23:49:24 +0800

    develop

---
+++ app/addons/export_report/schemas/menu/menu.post.php
@@ -0,0 +1,10 @@
+<?php
+
+use \Tygh\Registry;
+
+$schema['top']['administration']['items']['export_data']['subitems']['report_order_full_detail'] = array(
+	'href' => 'exim.export?section=report_order_full_detail',
+  'position' => 500,
+);
+
+return $schema;
